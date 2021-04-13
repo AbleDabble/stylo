71fedb84ff8e7790e45230b438cfbb4bf8260594
AndyE <aenders@uwyo.edu>
2021-04-13 09:44:29 -0600

    can now add individual userrs to the corpus

--- code/Twitterscrape.py
+++ code/Twitterscrape.py
@@ -3,6 +3,7 @@
 
 # Then in anaconda package manager, make sure it includes the conda-forge channel
 import tweepy
+import io
 
 # Uses personal API keys to access, DONT FORGET TO CHANGE
 # BEFORE PUSHING!!
@@ -21,18 +22,32 @@ api = tweepy.API(auth)
 user_list = ["TheEpic_Ace", "MountainWest", "markiplier"]
 
 
+# This Function is for adding individual users to the corpus!
 def getTweets(username):
-    timeline2 = api.user_timeline(screen_name=username, count=5, tweet_mode = 'extended')
+    path = '../corpora/twitter_corpus/'
+    save_path = path + username + ".txt"
+    save = ""
+    count = 0
+
+    timeline2 = api.user_timeline(screen_name=username, count=60, tweet_mode='extended')
     for tweets in timeline2:
-        print(username)
-        print(tweets.full_text)
-        print("\n")
+        save += "\n\n"
+        save += tweets.full_text
+        count += 1
+        if count >= 60:
+            break
+        with io.open(save_path, "w", encoding="utf-8") as w:
+            w.write(save)
+
+    # print(username)
+    # print(tweets.full_text)
+    # print("\n")
+
 
 user = "TheEpic_Ace"
 getTweets(user)
 
-
-#for user in user_list:
+# for user in user_list:
 #    user_id = user
 #    timeline1 = api.user_timeline(screen_name=user_id, count=5, tweet_mode='extended')
 #    for tweet in timeline1:
