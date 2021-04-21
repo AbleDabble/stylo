26b9e876caeec23563a82f3b08cdefc3e7a70587
AbleDabble <wcary22@gmail.com>
2021-04-21 13:44:31 -0600

    Changes and removals
    Remove:
    StyloModel
    __init__.py
    
    Changes:
    * IdentModel removed most of function
    * IdentProfile Overwrote extract function for IdentText and IdentProfile Class
    
    TODO: Need to change how the class label is decided based on current files

--- code/IdentModel.py
+++ code/IdentModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from IdentProfile import IdentProfile, IdentText
-from reddit_Scrape import redditScraper
+from .reddit_Scrape import redditScraper
 import os
 from sklearn.svm import SVC
 from EntropyDiscretization import EntropyDiscretization as ED
@@ -9,38 +9,46 @@ from MIFS import MIFS
 from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
+def split_x_y(df, numpy=False):
+    y = df["target"].copy()
+    x = df.drop("target", axis=1)
+    if numpy:
+        x = x.to_numpy()
+        y = y.to_numpy()
+    return x, y
+
 
 def start_identification_reddit(user_list, text):
     rs = redditScraper()
-    current_users = [user[:-4] for user in os.listdir('../corpora/reddit_corpus') if user.endswith('.txt')][:50]
-    user_paths = []
-    current_users_length = 0
-    user_label_names = {}
-    for user in user_list:
-        if user not in current_users:
-            ret = rs.getUserComments(user)
-            if ret == 0:
-                print(f'Problem downloading comments for user {user}')
-        user_paths.append(ret) 
     user_profiles = []
-    current_users = ['../corpora/reddit_corpus/' + user + '.txt' for user in current_users]
-    current_users += user_list
-    for i, path in enumerate(current_users):
-        user_profiles.append(IdentProfile(path, current_class=current_users_length).create_profile())
-        user_label_names[i] = path
-    text_df = IdentText('', text)
-    svc = SVC(kernel='poly')
-    df = pd.concat(user_profiles).reset_index()
-    target = df['target']
-    df = df.drop('target', axis=0)
-    pipe = Pipeline([("MinMaxScaler", MinMaxScaler()), ('ED', ED()), ('MIFS', MIFS()), ('SVC', SVC(kernel='poly', class_weight={0:1, 1:100}, degree=3))])
-    pipe.fit(df, target)
-    prediction = pipe.predict(text_df)
+
+    downloaded_users = {user[:-4] for user in os.listdir('../corpora/reddit_corpus/') if user.endswith('.txt')}
+    # Download new users or if already downloaded add them 
+    # their profile to the list
+    for user in user_list: 
+      if user in downloaded_users:
+        continue
+      path = rs.getUserComments(user)
+      ip = IdentProfile(path)
+      
+    
+
+
+
+
+    x, y = split_x_y(df, numpy=True)
+    print('y',y)
+    pipe = Pipeline([
+        ("MinMaxScaler", MinMaxScaler()),
+        ('ED', ED()),
+        ('MIFS', MIFS()),
+        ('SVC', SVC(kernel='poly', degree=3))]) # type: ignore
+    pipe.fit(x, y)
+    x_test = text_df.to_numpy()
+    prediction = pipe.predict(x_test)
     print(user_label_names[prediction])
     return user_label_names[prediction]
 
 
 
-
-        
-start_identification_reddit(['Baerog'],"This is a text block. This should be about 350 characters and probably will not be that large. Therefor it will be probably be smaller or maybe even bigger I don't know. It is hard to tell if this is 350 characters. More words and more text and more and more and more. This is the way things are go go. Power puff girls.")
+start_identification_reddit(['Baerog'],"This is a text block. This should be about 350 characters and probably will not be that large. Therefor it will be probably be smaller or maybe even bigger I don't know. It is hard to tell if this is 350 characters. More words and more text and more and more and more. This is the way things are go go. Power puff girls. There is more letters to add. Now we will continue to add more letters. This will have more letters and characters. There need to be a lot of fucking characters")
--- code/StyloModel.py
+++
@@ -1,2 +0,0 @@
-from Profile import Profile
-
