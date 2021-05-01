341048d44f64f1bf6909faff0a83994bdedc2f63
AbleDabble <wcary22@gmail.com>
2021-05-01 15:29:07 -0600

    Move removes and reset Json
    Since we are removing the downloaded file at the end we need to reset the json
    and delete the downloaded test file

--- code/tests.py
+++ code/tests.py
@@ -1,6 +1,16 @@
 from verifModel import start_verification_reddit, start_verification_twitter
 from IdentModel import start_identification_twitter, start_identification_reddit
 import os
+import json
+
+twitter_config_path = '../config/labels_twitter.json'
+reddit_config_path = '../config/labels.json'
+with open(twitter_config_path, 'r') as f:
+    twitter_config = json.load(f) 
+
+with open(reddit_config_path, 'r') as f:
+    reddit_config = json.load(f)
+
 
 print('TWITTER VERIFICATION')
 start_verification_twitter('CNN', 'jimcramer')
@@ -8,25 +18,33 @@ start_verification_twitter('CNN', 'jimcramer')
 print('DOWNLOADING TEST')
 print('\tAttempting to download Michael Malice')
 
-os.remove('../corpora/twitter_corpus/michaelmalice.txt')
 start_verification_twitter('CNN', 'michaelmalice')
+
 try:
     print('File size:', os.path.getsize('../corpora/twitter_corpus/michaelmalice.txt'))
+    os.remove('../corpora/twitter_corpus/michaelmalice.txt')
 except OSError:
     print('ERROR: File Does not exist or could not be found')
+
+with open(twitter_config_path, 'w') as f:
+    json.dump(twitter_config, f)
 
 print('REDDIT VERIFICATION')
 start_verification_reddit('Baerog', 'chafedogg')
 
 print('DOWNLOADING TEST')
 print('\tAttempting to download luncheroo')
-os.remove('../corpora/reddit_corpus/luncheroo.txt')
 start_verification_reddit('Baerog', 'luncheroo')
 
 try:
     print('File size:', os.path.getsize('../corpora/reddit_corpus/luncheroo.txt'))
+    os.remove('../corpora/reddit_corpus/luncheroo.txt')
 except OSError:
     print('ERROR: file does not exist or could not be found')
+
+with open(reddit_config_path, 'w') as f:
+    json.dump(reddit_config, f)
+
 
 print('IDENTIFICATION TWITTER')
 
@@ -40,4 +58,6 @@ print('REDDIT IDENTIFICATION')
 text = 'I am staring down the empty abyss of life and have lost all meaning. Penniless and at the end of my supply of the drug which alone gives life meaning, I will casy myself from this garret window to the squalid street below. Do not judge me for my addiction to morphine. It is my hope that after these hastily scrawled pages that you understand'
 
 start_identification_reddit(['Baerog', '4Darco','artchang','externality'], text)
+
+
 
